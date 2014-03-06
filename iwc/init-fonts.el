04880005c4609ad7a099fec5d0997dc15e61554c
iwc <iwc@superboom.org>
2014-03-06 18:00:55 -0500

    Set dejavu for linux

--- iwc/init-fonts.el
+++ iwc/init-fonts.el
@@ -4,9 +4,11 @@
 
 (if (eq system-type 'darwin)
     (set-face-attribute 'default nil :family "Source Code Pro" :height 115)
-    (setq mac-allow-anti-aliasing t)
+  (setq mac-allow-anti-aliasing t)
   (and window-system
-       (set-face-font 'default "-misc-fixed-medium-*-normal-*-*-130-*-*-*-*-*-*"))
-  )
+       (set-face-font 'default "-misc-fixed-medium-*-normal-*-*-130-*-*-*-*-*-*")))
 
+(if (eq system-type 'gnu/linux)
+    (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 100)
+    )
 (provide 'init-fonts)
