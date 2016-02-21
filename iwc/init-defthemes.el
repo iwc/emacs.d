a0cfe6f45621737078248df19c5e987ca1dc6e75
Irwin Coleman <iwc@superboom.org>
2016-02-21 16:19:58 -0500

    Tweaked theme settings atom-one-dark and a subatomic256

--- iwc/init-defthemes.el
+++ iwc/init-defthemes.el
@@ -3,6 +3,9 @@
 ;;"~/.emacs.d/iwc/defthemes")
 
 ;; load the theme
-(load-theme 'mustang-iwc t)
+;(load-theme 'mustang-iwc t)
+(if (not window-system)
+(load-theme 'subatomic256 t)    
+(load-theme 'atom-one-dark t))
 
 (provide 'init-defthemes)
