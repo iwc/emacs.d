6991f74a49832eb513f4f19878fde97f6a48481c
Irwin Coleman <iwc@superboom.org>
2018-03-19 14:09:15 -0400

    Moved to melpa version of doom-themes.

--- iwc/init-defthemes.el
+++ iwc/init-defthemes.el
@@ -3,9 +3,9 @@
 
 ;; load the theme
 ;; add the defthemes dir to the load path so that we can load doom-themes.el
-(add-to-list 'load-path (expand-file-name "iwc/defthemes" user-emacs-directory))
+;(add-to-list 'load-path (expand-file-name "iwc/defthemes" user-emacs-directory))
 
-(require 'doom-theme-config)
+;(require 'doom-theme-config)
 
 ;;(require-package 'solarized-theme)
 ;;(require 'solarized-theme-config)
@@ -13,4 +13,22 @@
 
 ;;(require-package 'flatui-theme)
 ;;(load-theme 'flatui t)
+(require-package 'doom-themes)
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-one t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Enable custom neotree theme
+;(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+
 (provide 'init-defthemes)
