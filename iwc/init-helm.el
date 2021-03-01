92ed6d9ab413022ebc40476c0ab69601892c064b
Irwin Coleman <icoleman@libro.fm>
2021-03-01 09:56:05 -0500

    helm-grep configuration

--- iwc/init-helm.el
+++ iwc/init-helm.el
@@ -6,4 +6,7 @@
 (setq helm-semantic-fuzzy-match t
       helm-imenu-fuzzy-match t)
 
+(setq helm-grep-ag-command "ag --line-numbers -S --hidden --color --color-match '31;43' --nogroup %s %s %s")
+(setq helm-grep-ag-pipe-cmd-switches '("--color-match '31;43'"))
+
 (provide 'init-helm)
